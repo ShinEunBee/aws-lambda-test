19a3da0abfa16cd07ff540ffbe9b13cd8296068e
GGang <eun1018bee@naver.com>
2025-09-18 16:37:48 +0900

    DynamoDB POST 체험

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import { useState } from "react";
 import "./App.css";
 
+type List = {
+  id: string;
+  text: string;
+};
+
 function App() {
-  const [text, setText] = useState("-");
+  const [list, setList] = useState<List[]>([]);
   const testUrl = import.meta.env.VITE_TEST_FUNCTION_URL;
 
   async function testFunc() {
-    const res = await fetch(testUrl, { method: "GET" });
-    const data = await res.json();
-    setText(data.time);
+    const res = await fetch(`${testUrl}/list`).then((r) => r.json());
+
+    setList(res.items);
   }
 
   return (
     <>
       <button onClick={testFunc}>다시 호출</button>
-      <div>{text}</div>
+
+      <div>
+        {list.map((line, index) => (
+          <li key={index}>{line.text}</li>
+        ))}
+      </div>
     </>
   );
 }
